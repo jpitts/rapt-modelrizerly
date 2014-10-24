b5d8604a5e2c740e2fefeec2ff6d355e59f780a6
jpitts <james.pitts@gmail.com>
2014-10-24 08:27:42 -0700

    Added an index.js file. Updated the .gitignore to include the devel directory.

---
+++ index.js
@@ -0,0 +1,9 @@
+/*!
+ * Cipher
+ * Copyright (c) 2012 Max Seiden
+ * Currently maintained by Jamie Pitts
+ * MIT Licensed
+*/
+
+// Serve as a proxy to the actual library
+module.exports = exports = require("./lib/modelrizerly");
